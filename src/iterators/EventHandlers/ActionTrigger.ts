2080a90f05a2a820a0d5e6c1ac9195f0904ea517
R. Beltran <rbeltran8000@gmail.com>
2022-07-17 12:41:44 +0200

    ActionTrigger added

---
+++ src/iterators/EventHandlers/ActionTrigger.ts
@@ -0,0 +1,26 @@
+
+export type EventData = object | any
+
+export class ActionTrigger {
+
+    readonly ActionName: string
+
+    private eventTarget: EventTarget = new EventTarget()
+
+    constructor(actionName: string = 'Invoke') {
+        this.ActionName = actionName
+    }
+
+    Add(this: ActionTrigger, actionHandler: any) {
+        this.eventTarget.addEventListener(this.ActionName, actionHandler)
+    }
+
+    Remove(this: ActionTrigger, actionHandler: any) {
+        this.eventTarget.removeEventListener(this.ActionName, actionHandler)
+    }
+
+    Invoke(this: ActionTrigger, detail?: EventData) {
+        const ev = new CustomEvent(this.ActionName, { detail: detail })
+        this.eventTarget.dispatchEvent(ev)
+    }
+}
