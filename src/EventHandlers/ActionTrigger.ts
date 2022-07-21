23eb939f2796a3bea3ad7ff4ba11a5c0ebe68add
R. Beltran <rbeltran8000@gmail.com>
2022-07-21 22:04:04 +0200

    fix ActionTrigger

--- src/EventHandlers/ActionTrigger.ts
+++ src/EventHandlers/ActionTrigger.ts
@@ -2,24 +2,32 @@
 export type EventData = object | any
 
 export class ActionTrigger {
-  readonly ActionName: string
+  public ActionName: string
 
-  private readonly eventTarget: EventTarget = new EventTarget()
+  addEventListener: (type: string, callback: EventListenerOrEventListenerObject | null, options?: boolean | AddEventListenerOptions | undefined) => void
+  removeEventListener: (type: string, callback: EventListenerOrEventListenerObject | null, options?: boolean | EventListenerOptions | undefined) => void
+  dispatchEvent: (event: Event) => boolean
 
   constructor (actionName: string = 'Invoke') {
     this.ActionName = actionName
+    let eventTarget = document.createTextNode(' ')
+
+    // Pass EventTarget interface calls to DOM EventTarget object
+    this.addEventListener = eventTarget.addEventListener.bind(eventTarget);
+    this.removeEventListener = eventTarget.removeEventListener.bind(eventTarget);
+    this.dispatchEvent = eventTarget.dispatchEvent.bind(eventTarget);
   }
 
-  Add (this: ActionTrigger, actionHandler: any) {
-    this.eventTarget.addEventListener(this.ActionName, actionHandler)
+  Add (this: ActionTrigger, actionHandler: EventListenerOrEventListenerObject | null) {
+    this.addEventListener(this.ActionName, actionHandler)
   }
 
-  Remove (this: ActionTrigger, actionHandler: any) {
-    this.eventTarget.removeEventListener(this.ActionName, actionHandler)
+  Remove (this: ActionTrigger, actionHandler: EventListenerOrEventListenerObject | null) {
+    this.removeEventListener(this.ActionName, actionHandler)
   }
 
-  Invoke (this: ActionTrigger, detail?: EventData) {
-    const ev = new CustomEvent(this.ActionName, { detail: detail })
-    this.eventTarget.dispatchEvent(ev)
+  Invoke (this: ActionTrigger /*, detail?: EventData*/) {
+    // const ev = new CustomEvent(this.ActionName, { detail: detail || "EMPTY" })
+    this.dispatchEvent(new Event(this.ActionName, { cancelable: false }))
   }
 }
